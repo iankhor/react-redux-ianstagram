2b26a89f8ca5baf4e02bf058c9bd37938aa5943d
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-05 23:23:53 +1100

    added react.cloneElement to get props from parents component

--- client/components/Main.js
+++ client/components/Main.js
@@ -4,11 +4,14 @@ import { Link } from 'react-router'
 export default class Main extends Component {
     render(){
         return(
-            <div className="main">   
+            <div>   
                 <h1>
-                    <Link to="/">Reduxstagram</Link>
-                    {this.props.children}
+                    <Link to="/">Ian-stagram</Link>
                 </h1>
+
+            {/* getting props from parents components ie: react router etc */}
+            {React.cloneElement(this.props.children, this.props)}
+
             </div>
         )
     }
