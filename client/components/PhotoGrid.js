a5317949d4cb29b0065f712402c6db795bda5c6d
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-06 22:05:49 +1100

    updated photogrid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,11 @@ export default class PhotoGrid extends Component {
     render(){
         return(
             <div className="photo-grid">   
-                {this.props.posts.map( (post,index) => <Photo /> )}
+                {this.props.posts.map( (post,index) => 
+                                        <Photo {...this.props} 
+                                               key={index} 
+                                               index={index}
+                                               post={post} /> )}
             </div>
         )
     }
